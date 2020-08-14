1f45af71d405231d2dca681d229bc4f86d62419e
jjang <cktpdnd871@gmail.com>
2020-08-14 17:48:23 +0900

    Add usercontroller.java

---
+++ usercontroller.java
@@ -0,0 +1,10 @@
+@controller
+public class userController{
+
+	// 로그인 페이지로 이동
+	@RequestMapping("/user/login.do")
+	public String login(){
+		logger.info("경로 : login");
+	return "user/login";	
+	}
+}
